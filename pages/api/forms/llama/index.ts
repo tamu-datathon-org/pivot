a1d6afa8d30e5820f0e56c895910caadb89039f3
Samarth Dave <samarthdave@users.noreply.github.com>
2020-09-19 00:30:50 -0500

    Test typeform llama

---
+++ pages/api/forms/llama/index.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { InfoBuilderDataFields } from "../../../../utils/info-builder/model";
+import { buildInfo } from "../../../../utils/info-builder";
+import { genGatekeeperUserInfo } from "../../../../utils/auth";
+
+const formUrl = "https://datathon.typeform.com/to/qcNWGRrM";
+const queryParamMapping = {
+  "name": InfoBuilderDataFields.GATEKEEPER_FIRST_NAME,
+};
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  try {
+    const gatekeeperResponse = await genGatekeeperUserInfo(
+      req.cookies.accessToken
+    );
+
+    const queryParams = buildInfo(queryParamMapping, {
+      gatekeeperData: gatekeeperResponse.data,
+    });
+    const redirectUrl = new URL(formUrl);
+    // Add required query params based on build info object.
+    Object.keys(queryParams).forEach((key) =>
+      redirectUrl.searchParams.set(key, queryParams[key])
+    );
+    res.redirect(redirectUrl.toString());
+  } catch (e) {
+    return res.redirect(encodeURI(`${process.env.GATEKEEPER_URL}/login?r=${req.url}`));
+  }
+};
+
